b8b9c122200a602762a78a907308cfc6589a1a3a
LL <lianidaz@gmail.com>
2018-10-05 15:59:07 +0300

    aweg

--- routers/flat.js
+++ routers/flat.js
@@ -7,6 +7,7 @@ r.post("/butt", (req, res) => {
     models.Device.findOne({ _id: dev.output_id }, (err, output) => {
       if (err) throw err;
       output.state = !output.state;
+      output.save()
     });
   });
 });
