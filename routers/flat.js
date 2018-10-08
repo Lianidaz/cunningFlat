114d7287c99b01dc0980ba166e592e70234e470c
Leonid Levin <ll@weedbox.skatebanana.local>
2018-10-08 23:56:34 +0300

    stats?

--- routers/flat.js
+++ routers/flat.js
@@ -11,4 +11,31 @@ r.post("/butt", (req, res) => {
   });
 });
 
+r.get("/state", (req, res) => {
+  res.send(
+    models.Log.findOne({}, (err, data) => {
+      if (err) res.send(err);
+      res.send(data);
+    })
+  );
+});
+
+r.get("/state/:room", (req, res) => {
+  models.Room.findOne({ nam: req.params.room }, (err, data) => {
+    if (err) res.send(err);
+    models.Log.findOne({}, (err, entry) => {
+      let arr = [];
+      if (err) res.send(err);
+      for (d of data.devices) {
+        for (p of entry.states) {
+          if (d._id === p._id) {
+            arr.push(p);
+          }
+        }
+      }
+      res.send(arr);
+    });
+  });
+});
+
 module.exports = r;
