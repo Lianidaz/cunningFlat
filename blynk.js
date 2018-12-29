6930ac3a90c5347106b359353993b278392781e5
Leonid <leolevin@internal.cb.com>
2018-12-29 12:10:06 +0300

    +jjj

--- blynk.js
+++ blynk.js
@@ -1,11 +1,53 @@
 const Blynk = require("blynk-library");
+require("dotenv").config();
+const request = require("request");
 
-const AUTH = "4b11ff8fb3924b2e998057bd3ea88741";
+const AUTH = process.env.AUTH;
 
-const blynk = new Blynk.Blynk(AUTH, "etherlab.online", 7080);
+const blynk = new Blynk.Blynk(AUTH, {
+  connector: new Blynk.TcpClient({
+    addr: "etherlab.online",
+    port: 7080
+  })
+});
 
 let v1 = new blynk.VirtualPin(1);
+let v0 = new blynk.VirtualPin(0);
+let v2 = new blynk.VirtualPin(2);
+let v3 = new blynk.VirtualPin(3);
+let v4 = new blynk.VirtualPin(4);
 
 v1.on("write", par => {
   console.log(par);
 });
+
+let status = (par, pin) => {
+  let job;
+  switch (par) {
+    case "1":
+      job = "autotests execution";
+      break;
+    case "2":
+      job = "NOMAD_CB_TEST";
+      break;
+    case "3":
+      job = "run_datasettool";
+      break;
+  }
+
+  request.get(
+    "http://" +
+      process.env.JCRED +
+      "@build:8080/job/" +
+      job +
+      "/lastBuild/api/json?tree=result",
+    (err, res, body) => {
+      console.log(job + ":\n" + JSON.parse(body).result + "\n\n");
+      pin.write(job + ": " + JSON.parse(body).result);
+    }
+  );
+};
+
+v2.on("read", () => status("1", v2));
+v3.on("read", () => status("2", v3));
+v4.on("read", () => status("3", v4));
